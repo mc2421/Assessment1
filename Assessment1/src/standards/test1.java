a0f5412944ec785c53a2d7d3522460af93f52d63
mc307009 <michaelcurtis2421@gmail.com>
2017-09-01 08:36:20 -0500

    Updated

--- Assessment1/src/standards/test1.java
+++ Assessment1/src/standards/test1.java
@@ -1,10 +1,12 @@
 package standards;
 
+import javax.swing.JOptionPane;
+
 public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JOptionPane.showMessageDialog(null, "says something funny");
 	}
 
 }
--- Assessment1/src/standards/test3.java
+++ Assessment1/src/standards/test3.java
@@ -1,10 +1,15 @@
 package standards;
 
+import javax.swing.JOptionPane;
+
 public class test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		JOptionPane.showMessageDialog(null, "Hi There");
+		
+		//System.out.println("What up");
 	}
 
 }
